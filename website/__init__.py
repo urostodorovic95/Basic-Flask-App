6bbdcecd0a39a21db8c719d3c7e2b7b7b855af06
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-11-10 18:42:12 +0100

    Add routes and basic viewfunctions

--- website/__init__.py
+++ website/__init__.py
@@ -10,5 +10,13 @@ def create_app():
     Return the app object."""
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('APP_KEY')  # generate with secrets.token_hex(16)
+    
+    # import view and auth routes
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
 
---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route("/login")
+def login():
+    return "login"
+
+
+@auth.route("/logout")
+def logout():
+    return "logout"
+
+
+@auth.route("/sign-up")
+def signup():
+    return "sign up"
