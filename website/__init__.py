37d80964748a1d3d0162e6bed90ea9a17a6930f7
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-11-10 17:36:38 +0100

    Initialise the app

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def create_app():
+    """Create a Flask app.
+    Return the app object."""
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.get('APP_KEY')  # generate with secrets.token_hex(16)
+    return app
+
