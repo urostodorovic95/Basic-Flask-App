c3f53ebbce5418b11ed78671d410d30a0ad8e5c6
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-11-12 15:40:06 +0100

    Save new users to database

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .forms import LogIn, SignUp
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -23,6 +25,13 @@ def logout():
 def signup():
     form = SignUp()
     if form.validate_on_submit():
+        new_user = User(
+            username=form.first_name.data,
+            email=form.email.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print(f"User {db.session.get(User, new_user.id)} Added!")
         return redirect(url_for('views.hp', user_signed=True))
         
     return render_template('signup.html', form=form, form_check=True)
