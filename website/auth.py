945e124737f38690349ac7bf5b9f7b24c070ff8c
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-11-12 13:35:45 +0100

    Implement a SignUp WTF Form

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .forms import LogIn
+from .forms import LogIn, SignUp
 
 auth = Blueprint('auth', __name__)
 
@@ -21,13 +21,9 @@ def logout():
 
 @auth.route("sign-up", methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        print(request.form)
-        email = request.form.get('inputemail')
-        first_name = request.form.get('fname')
-        password = request.form.get('password')
-        password_repeat = request.form.get('confirm-password')
-        if request.form.get('rememberme') == 'on':
-            remember = True
-    return render_template('signup.html')
+    form = SignUp()
+    if form.validate_on_submit():
+        return redirect(url_for('views.hp'))
+        
+    return render_template('signup.html', form=form)
 
--- website/forms.py
+++ website/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class LogIn(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+
+class SignUp(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired(), Length(min=8, max=20), EqualTo('password')])
+    submit = SubmitField('Create Account')
 
